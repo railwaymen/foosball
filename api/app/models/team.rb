38725f291ace6acc881b297d601b59d626f43934
Piotr Soboń <piotr.sobon@posbistro.com>
2018-10-25 16:20:12 +0200

    Changed teams relation

--- api/app/models/team.rb
+++ api/app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ApplicationRecord
   belongs_to :tournament
-  belongs_to :attacker, class_name: 'TournamentPlayer'
-  belongs_to :defender, class_name: 'TournamentPlayer'
+  belongs_to :attacker, class_name: 'Player'
+  belongs_to :defender, class_name: 'Player'
 end
