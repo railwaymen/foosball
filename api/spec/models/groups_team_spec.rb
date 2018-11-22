c0637d2819817d6043fcc14cefebe7b9e2c9d69e
Piotr Soboń <piotr.sobon@posbistro.com>
2018-11-22 14:59:12 +0100

    Added some models tests

---
+++ api/spec/models/groups_team_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe GroupsTeam, type: :model do
+  it { should validate_numericality_of(:points).is_greater_than_or_equal_to(0) }
+  it { should validate_numericality_of(:goals_against).is_greater_than_or_equal_to(0) }
+  it { should validate_numericality_of(:goals_for).is_greater_than_or_equal_to(0) }
+  it { should validate_numericality_of(:played).is_greater_than_or_equal_to(0) }
+  it { should validate_numericality_of(:goal_difference) }
+end
